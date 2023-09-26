443364b00b2f8b3d5ec63e0d3ec46c257c2a2eb6
bernardoenock <bernardoenockgm@gmail.com>
2023-09-26 14:57:52 -0300

    Updates

---
+++ precache-manifest.78c3a180fe9a3a3bfe0ab226251cb3ad.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4298f8f7a870c639c3a3ae6ba1194f",
+    "url": "/smart_bank_alura/index.html"
+  },
+  {
+    "revision": "331dd94728cd47fbc554",
+    "url": "/smart_bank_alura/static/js/2.f758fda7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/smart_bank_alura/static/js/2.f758fda7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9628cb63d6fb3733ab1",
+    "url": "/smart_bank_alura/static/js/main.797eaa27.chunk.js"
+  },
+  {
+    "revision": "e38d0838fa4c62d0b989",
+    "url": "/smart_bank_alura/static/js/runtime-main.5c825edf.js"
+  },
+  {
+    "revision": "44dbaa78fb2ed240792c9e7a424b0ea0",
+    "url": "/smart_bank_alura/static/media/alimentacao.44dbaa78.svg"
+  },
+  {
+    "revision": "f3a2bff49642ca68b3841fde422295cb",
+    "url": "/smart_bank_alura/static/media/bank_logo.f3a2bff4.svg"
+  },
+  {
+    "revision": "b97c300bed11302a8687c16e07aa89e5",
+    "url": "/smart_bank_alura/static/media/dinheiro.b97c300b.svg"
+  },
+  {
+    "revision": "acd41ceb3366a71c61b7ed3d5df83682",
+    "url": "/smart_bank_alura/static/media/olho.acd41ceb.svg"
+  },
+  {
+    "revision": "17e95f3cc45dc23ffda7f4d29dc137df",
+    "url": "/smart_bank_alura/static/media/outros.17e95f3c.svg"
+  },
+  {
+    "revision": "134d853a95b2b30676da2c63cda2eeaf",
+    "url": "/smart_bank_alura/static/media/privado.134d853a.svg"
+  },
+  {
+    "revision": "605ef9ca7e70a1bb568ffcec443d05f6",
+    "url": "/smart_bank_alura/static/media/saude.605ef9ca.svg"
+  },
+  {
+    "revision": "0981f5bfd5f197c52e678c3c0616cd81",
+    "url": "/smart_bank_alura/static/media/themeOff.0981f5bf.svg"
+  },
+  {
+    "revision": "75ea28146734a5ea6593dab45d272a90",
+    "url": "/smart_bank_alura/static/media/themeOn.75ea2814.svg"
+  },
+  {
+    "revision": "15a4ed7310630a72e21a8054897c3a51",
+    "url": "/smart_bank_alura/static/media/transporte.15a4ed73.svg"
+  },
+  {
+    "revision": "59035811601fe0a607827eb6408e14ef",
+    "url": "/smart_bank_alura/static/media/utilidades.59035811.svg"
+  }
+]);
